bffd3ddb6bc756fff166f3d7ab5e4b2f70a31f12
flavio-valle <flavio.valle2006@gmail.com>
2025-05-13 22:34:19 -0300

    teste pipeline

--- src/main/java/com/fiap/challenge/quod/antifraude_backend/config/SecurityConfig.java
+++ src/main/java/com/fiap/challenge/quod/antifraude_backend/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 public class SecurityConfig {
 
+
     private final TokenAuthenticationFilter tokenFilter;
 
     public SecurityConfig(TokenAuthenticationFilter tokenFilter) {
